a803c7f4cc702a4e778c678a457e849dbaf4c4a3
dirouc <dirouc@inbox.ru>
2017-09-22 18:43:06 +0300

    small fix

--- start.cmd
+++ start.cmd
@@ -1,5 +1,5 @@
 @echo off
-TITLE PocketMine-MP server software for Minecraft: Pocket Edition
+TITLE Prismarine server software for Minecraft: Bedrock Edition
 cd /d %~dp0
 
 if exist bin\php\php.exe (
@@ -9,20 +9,28 @@ if exist bin\php\php.exe (
 	set PHP_BINARY=php
 )
 
-if exist PocketMine-MP.phar (
-	set POCKETMINE_FILE=PocketMine-MP.phar
+if exist Prismarine*.phar (
+	set POCKETMINE_FILE=Prismarine*.phar
 ) else (
-	if exist src\pocketmine\PocketMine.php (
-		set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+	if exist Prismarine.phar (
+		set POCKETMINE_FILE=Prismarine.phar
 	) else (
-		echo "Couldn't find a valid PocketMine-MP installation"
-		pause
-		exit 1
+		if exist PocketMine-MP.phar (
+			set POCKETMINE_FILE=PocketMine-MP.phar
+		) else (
+		    if exist src\pocketmine\PocketMine.php (
+		        set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+			) else (
+		        echo "[ERROR] Couldn't find a valid Prismarine installation."
+		        pause
+		        exit 8
+		    )
+	    )
 	)
 )
 
 if exist bin\mintty.exe (
-	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "Prismarine" -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
 ) else (
 	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
 )
