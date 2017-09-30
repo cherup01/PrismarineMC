1893a2b49454f5f5fc5817e6cfa004ba417d7ee0
Encritary <mrgenga537@gmail.com>
2017-09-30 16:54:43 +0500

    Fix Ender Pearls and Eggs

--- src/pocketmine/entity/EnderPearl.php
+++ src/pocketmine/entity/EnderPearl.php
@@ -88,12 +88,8 @@ class EnderPearl extends Projectile{
 		$pk = new AddEntityPacket();
 		$pk->type = EnderPearl::NETWORK_ID;
 		$pk->entityRuntimeId = $this->getId();
-		$pk->x = $this->x;
-		$pk->y = $this->y;
-		$pk->z = $this->z;
-		$pk->speedX = $this->motionX;
-		$pk->speedY = $this->motionY;
-		$pk->speedZ = $this->motionZ;
+		$pk->position = $this->asVector3();
+		$pk->motion = $this->getMotion();
 		$pk->metadata = $this->dataProperties;
 		$player->dataPacket($pk);
 
