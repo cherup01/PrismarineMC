92aed1c393062554e2b9d28c1088920c0a71df0e
Encritary <mrgenga537@gmail.com>
2017-10-01 12:29:17 +0500

    Fix FloatingTextParticle

--- src/pocketmine/level/particle/FloatingTextParticle.php
+++ src/pocketmine/level/particle/FloatingTextParticle.php
@@ -83,7 +83,7 @@ class FloatingTextParticle extends Particle{
 			$pk->uuid = UUID::fromRandom();
 			$pk->username = $this->title;
 			$pk->entityRuntimeId = $this->entityId;
-			$pk->position = new Vector3(this->x, $this->y - 0.50, $this->z);
+			$pk->position = new Vector3($this->x, $this->y - 0.50, $this->z);
 			$pk->item = Item::get(Item::AIR);
 			$flags = (
 				(1 << Entity::DATA_FLAG_CAN_SHOW_NAMETAG) |
