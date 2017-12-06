a354e380b852821da349bf06e6e8703e40ba37e2
Encritary <mrgenga537@gmail.com>
2017-12-06 08:17:46 +0500

    Bump up version

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -79,7 +79,7 @@ namespace pocketmine {
 	use raklib\RakLib;
 
 	const NAME = "Prismarine";
-	const VERSION = "1.2.0";
+	const VERSION = "1.2.5";
 	const API_VERSION = "3.0.0";
 	const CODENAME = "Cake";
 
