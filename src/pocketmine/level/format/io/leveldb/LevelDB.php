ef953c8c04e866048485c1c73c78ce974473c86e
Encritary <mrgenga537@gmail.com>
2017-12-10 19:01:13 +0500

    Minecraft 1.2 LevelDB worlds support

--- src/pocketmine/level/format/io/leveldb/LevelDB.php
+++ src/pocketmine/level/format/io/leveldb/LevelDB.php
@@ -69,8 +69,8 @@ class LevelDB extends BaseLevelProvider{
 	const GENERATOR_INFINITE = 1;
 	const GENERATOR_FLAT = 2;
 
-	const CURRENT_STORAGE_VERSION = 5; //Current MCPE level format version
-	const CURRENT_LEVEL_CHUNK_VERSION = 4;
+	const CURRENT_STORAGE_VERSION = 6; //Current MCPE level format version
+	const CURRENT_LEVEL_CHUNK_VERSION = 7;
 	const CURRENT_LEVEL_SUBCHUNK_VERSION = 0;
 
 	/** @var Chunk[] */
@@ -94,12 +94,17 @@ class LevelDB extends BaseLevelProvider{
 			throw new LevelException("Invalid level.dat");
 		}
 
+		if(!defined('LEVELDB_ZLIB_RAW_COMPRESSION')){
+			throw new LevelException("Given version of php-leveldb doesn't support zlib raw compression");
+		}
+
 		$this->db = new \LevelDB($this->path . "/db", [
-			"compression" => LEVELDB_ZLIB_COMPRESSION
+			"compression" => LEVELDB_ZLIB_RAW_COMPRESSION
 		]);
 
-		if(isset($this->levelData->StorageVersion) and $this->levelData->StorageVersion->getValue() > self::CURRENT_STORAGE_VERSION){
-			throw new LevelException("Specified LevelDB world format version is newer than the version supported by the server");
+		$version = $this->levelData->getInt("StorageVersion", INT32_MAX, true);
+		if($version > self::CURRENT_STORAGE_VERSION){
+			throw new LevelException("Specified LevelDB world format version ($version) is not supported by " . \pocketmine\NAME);
 		}
 
 		if(!isset($this->levelData->generatorName)){
@@ -208,7 +213,7 @@ class LevelDB extends BaseLevelProvider{
 
 
 		$db = new \LevelDB($path . "/db", [
-			"compression" => LEVELDB_ZLIB_COMPRESSION
+			"compression" => LEVELDB_ZLIB_RAW_COMPRESSION
 		]);
 
 		if($generatorType === self::GENERATOR_FLAT and isset($options["preset"])){
@@ -312,6 +317,7 @@ class LevelDB extends BaseLevelProvider{
 			$binaryStream = new BinaryStream();
 
 			switch($chunkVersion){
+				case 7: //MC 1.2
 				case 4: //MCPE 1.1
 					//TODO: check beds
 				case 3: //MCPE 1.0
