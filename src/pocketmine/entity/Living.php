b95229c5f89147490e00954cbb4cf1fa0ecfd90c
Encritary <mrgenga537@gmail.com>
2018-01-11 15:11:48 +0500

    Some improvements to critical hits

--- src/pocketmine/entity/Living.php
+++ src/pocketmine/entity/Living.php
@@ -351,10 +351,10 @@ abstract class Living extends Entity implements Damageable{
 		if($source instanceof EntityDamageByEntityEvent){
 			$damager = $source->getDamager();
 			if($damager instanceof Player){
-				if($source->getCause() === EntityDamageEvent::CAUSE_ENTITY_ATTACK and !$damager->isFlying() and !$damager->isOnGround() and !$damager->isSprinting() and !$damager->hasEffect(Effect::BLINDNESS) and !$damager->isInsideOfWater()){
+				if($source->getCause() === EntityDamageEvent::CAUSE_ENTITY_ATTACK and !$damager->isFlying() and $damager->fallDistance > 0 and !$damager->isSprinting() and !$damager->hasEffect(Effect::BLINDNESS) and !$damager->isInsideOfWater()){
 					//Critical hit
-					for($i = 0; $i < 5; $i++){
-						$this->level->addParticle(new CriticalParticle(new Vector3($this->x + mt_rand(-15, 15) / 10, $this->y + mt_rand(0, 20) / 10, $this->z + mt_rand(-15, 15) / 10)));
+					for($i = 0; $i < 15; $i++){
+						$this->level->addParticle(new CriticalParticle(new Vector3($this->x + mt_rand(-10, 10) / 10, $this->y + mt_rand(0, 20) / 10, $this->z + mt_rand(-5, 5) / 10)));
 					}
 					$source->setDamage($source->getDamage() / 2, EntityDamageEvent::MODIFIER_CRITICAL);
 				}
